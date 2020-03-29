42206b7986324ee5b08addc7e45c8da064fcd651
George Hart <hamiltonhart@protonmail.com>
2020-03-29 16:37:19 -0700

    Notes App added to Django

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import datetime
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=300, null=False)
+    body_text = models.TextField(null=False)
+    created_date = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.title
