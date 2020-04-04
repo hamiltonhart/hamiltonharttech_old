a13dde8a9869fbf7316950d2158465818d9d128a
George Hart <hamiltonhart@protonmail.com>
2020-04-04 15:44:02 -0700

    React Config tweaked. View to call React index.html changed. urls.py file updated to call the edited view. Base package.json created.

--- hamiltonharttech/urls.py
+++ hamiltonharttech/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from .views import ReactAppView
+from .views import FrontendAppView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", ReactAppView.as_view()),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path(r'^', FrontendAppView.as_view()),
 ]
