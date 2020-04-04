0aa2b9f9be1928074a0df57e6c10d2e9d4ba57a5
George Hart <hamiltonhart@protonmail.com>
2020-04-04 16:18:55 -0700

    Updated BASE_DIR

--- hamiltonharttech/settings.py
+++ hamiltonharttech/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
