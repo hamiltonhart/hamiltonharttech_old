368657cd7ce0a93c1b7415ed0bd91b6df82ce8d6
George Hart <hamiltonhart@protonmail.com>
2020-04-04 17:17:20 -0700

    CORS_ORIGIN edited

--- hamiltonharttech/settings.py
+++ hamiltonharttech/settings.py
@@ -75,6 +75,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000", "https://hhtech.herokuapp.com"
 )
