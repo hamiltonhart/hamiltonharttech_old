f27e76bab7be4fd3cec76889a31ac3e45235f032
George Hart <hamiltonhart@protonmail.com>
2020-04-10 22:24:02 -0700

    Fixed overflow issue with Grid in NotesList

--- frontend/src/components/Global/PrimaryLayout.js
+++ frontend/src/components/Global/PrimaryLayout.js
@@ -5,14 +5,6 @@ import { makeStyles, Paper, Grid, Typography, Button } from "@material-ui/core";
 import { HHTLogo } from "../../svg/HHTLogo";
 
 const useStyles = makeStyles((theme) => ({
-  heading: {
-    padding: theme.spacing(2),
-    textTransform: "uppercase",
-    textDecoration: "none",
-    color: theme.palette.text.primary,
-    fontWeight: "600",
-    color: theme.palette.primary.main,
-  },
   logo: {
     scale: "60%",
     "& *": {
@@ -30,13 +22,16 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     alignItems: "center",
     position: "relative",
-    margin: "10px 15px 25px 15px",
+    margin: "10px 15px 15px 15px",
     borderRadius: "40px",
     padding: "10px 20px",
     backgroundColor: "transparent",
-    borderStyle: "solid",
-    borderWidth: "1px",
-    borderColor: theme.palette.primary.main,
+    boxShadow: theme.shadows[2],
+  },
+  mainDiv: {
+    maxWidth: theme.breakpoints.values.xl,
+    marginLeft: "auto",
+    marginRight: "auto",
   },
 }));
 
@@ -50,7 +45,7 @@ export const PrimaryLayout = ({ children }) => {
             <HHTLogo classes={classes.logo} />
           </Link>
         </div>
-        {children}
+        <div className={classes.mainDiv}>{children}</div>
       </Paper>
     </>
   );
--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -3,7 +3,14 @@ import { Link } from "@reach/router";
 import { Error, Loading } from "../components/Global";
 import { NoteListItem } from "../components/Notes";
 
-import { makeStyles, Typography, Button, Grid } from "@material-ui/core";
+import {
+  makeStyles,
+  Typography,
+  Button,
+  Grid,
+  useTheme,
+  useMediaQuery,
+} from "@material-ui/core";
 
 import { NOTES_QUERY } from "../gql";
 import { useQuery } from "@apollo/react-hooks";
@@ -11,8 +18,12 @@ import { IS_LOGGED_IN } from "../index.js";
 
 const useStyles = makeStyles((theme) => ({
   listContainer: {
-    padding: theme.spacing(3),
-    flexGrow: "1",
+    flexGrow: 1,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  heading: {
+    margin: "20px 0 20px 0",
   },
   listItem: {
     marginTop: theme.spacing(2),
@@ -30,45 +41,50 @@ export const NotesListPage = () => {
   const { data: loggedInData, error: loggedInError } = useQuery(IS_LOGGED_IN);
 
   const classes = useStyles();
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <>
-      <Typography variant="h3" align="center">
+      <Typography className={classes.heading} variant="h3" align="center">
         Notes
       </Typography>
-      <Grid container className={classes.listContainer} spacing={2}>
-        {loggedInError && <Error errorMessage={loggedInError.message} />}
-        {loggedInData && loggedInData.isLoggedIn && (
-          <Grid className={classes.createButton} item xs={12}>
-            <Button
-              color="primary"
-              variant="contained"
-              size="large"
-              component={Link}
-              to="/notes/create"
-            >
-              Add Note
-            </Button>
-          </Grid>
-        )}
-
-        {loading && (
-          <Grid item xs={12}>
-            <Loading />
-          </Grid>
-        )}
-        {error && <Error errorMessage={error.message} />}
-        {data &&
-          data.notes.map((note) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-              <NoteListItem
-                key={note.id}
-                note={note}
-                className={classes.listItem}
-              />
+      <div className={classes.listContainer}>
+        <Grid container spacing={2}>
+          {loggedInError && <Error errorMessage={loggedInError.message} />}
+          {loggedInData && loggedInData.isLoggedIn && (
+            <Grid className={classes.createButton} item xs={12}>
+              <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                component={Link}
+                to="/notes/create"
+                fullWidth={!isSmall}
+              >
+                Add Note
+              </Button>
             </Grid>
-          ))}
-      </Grid>
+          )}
+
+          {loading && (
+            <Grid item xs={12}>
+              <Loading />
+            </Grid>
+          )}
+          {error && <Error errorMessage={error.message} />}
+          {data &&
+            data.notes.map((note) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <NoteListItem
+                  key={note.id}
+                  note={note}
+                  className={classes.listItem}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </div>
     </>
   );
 };
